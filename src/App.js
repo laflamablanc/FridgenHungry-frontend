c020a583b2edecf0f913362cc38553b53221281e
Sean LaFlam <sean.laflam@gmail.com>
2020-11-05 15:48:37 -0500

    moved frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+class App extends React.Component {
+  render(){
+    return (
+      {}
+    )
+  }
+}
+
+export default App;
