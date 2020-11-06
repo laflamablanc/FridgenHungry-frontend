6f05bc1ab76f76290217c0d914899de1f28f5459
aribakhan97 <akhan41@binghamton.edu>
2020-11-06 17:32:21 -0500

    edited login and app

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 class Login extends React.Component {
-  render(){
+  state = {
+    isLoggedIn :false
+  }
+  loggedIn = () => {
+    this.setState({isLoggedIn :true})
+  }
+  render(){ 
     return(
-      <div> Login </div>
+      <div> 
+        <h2> WELCOME TO FRIDGEnHUNGRY! Please Login in to Continue </h2>
+        <form onSubmit = {this.loggedIn}>
+            <label for="Username">Username:</label><br/>
+            <input type="text" id="Username" name="Username"/><br/>
+            <label for="Password">Password:</label><br/>
+            <input type="text" id="Password" name="Password"/>
+            <input type="submit" id="submit" name="Submit"/>
+        </form>
+      </div>
     )
   }
 }
