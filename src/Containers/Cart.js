85c28cb71819ce055ccb674efb9b858427b71cfb
Sean LaFlam <sean.laflam@gmail.com>
2020-11-11 14:06:46 -0500

    Drag and Drop Sort of Working

--- src/Containers/Cart.js
+++ src/Containers/Cart.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import Ingredient from '../Components/Ingredient'
-import Grid from '@material-ui/core/Grid'
+import React from 'react';
+import Ingredient from '../Components/Ingredient';
+import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
+import { useDrop } from 'react-dnd';
+import { ItemTypes } from '../Utilities/items'
 
 const Cart = (props) => {
 
+  const[{isOver}, drop] = useDrop({
+    accept: ItemTypes.CARD,
+    collect: monitor => ({
+      isOver: !!monitor.isOver()
+    })
+  })
 
 
   const displayFood = () => {
@@ -28,8 +36,11 @@ const Cart = (props) => {
           backgroundImage: "url(" + "https://i.imgur.com/0IStklx.png" + ")",
           backgroundPosition: 'center',
           backgroundSize: "100% 100%",
-          backgroundRepeat: 'no-repeat'
-        }}>
+          backgroundRepeat: 'no-repeat',
+          border: isOver? "5px solid red" : "5px solid blue" 
+        }}
+        ref={drop}
+        >
         <h1>Cart</h1>
         <Grid container spacing={1}>
           {displayFood()}
